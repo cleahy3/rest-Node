0bc4e7f1eba80c8d9d91bd913cc0273aedce02d5
Callum Leahy <callumleahy23@googlemail.com>
2016-09-19 16:55:22 +0100

    Halfway through create post

--- app.js
+++ app.js
@@ -20,14 +20,30 @@ var cheeses = [
 		"strength": 1000
 	}
 ];
+app.use(function(req,res,next){
+	
+	var newCheese={
+		name : 'Cheese String',
+		strength :'0.5'
+	}
+	req.body=newCheese,
+	next();
+});
 app.get("/" , function(req,res,next) {
 	var body = req.body;
  // res.send({"time":req.time.toString()});
- 	 res.send(cheeses)});
+ 	 res.send(cheeses)
+});
 
-app.get("/cheese/:Parameter",function(req,res,next){
-	var id = req.params.Parameter;
+app.get("/cheese/:id",function(req,res,next){
+	var id = req.params.id;
 	res.send(cheeses[id-1]);
+	
+});
+app.post("/cheese/new",function(req,res,next){
+
+	cheeses.push(req.body);
+	res.send('Item Added '+req.body);
 });
 app.listen(3000 , function(){
   console.log('app running on port 3000');
